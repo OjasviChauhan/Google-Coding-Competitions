f51ec320d604968c1416650a28b442a1b5bca948
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-05-18 12:30:34 +0530

    Update Perfect Subarray.py

--- Kickstart 2020/Round C/Perfect Subarray.py
+++ Kickstart 2020/Round C/Perfect Subarray.py
@@ -7,7 +7,8 @@ for z in range(t):
     for i in range(0,n):
         for j in range(i+1,n+1):
             x = sum(A[i:j])
-            sr = math.sqrt(x)
-            if(sr - math.floor(sr) == 0):
-                c+=1
+            if(x >= 0):
+                sr = math.sqrt(x)
+                if(sr - math.floor(sr) == 0):
+                    c+=1                
     print("Case #"+str(z+1)+": "+str(c))   
