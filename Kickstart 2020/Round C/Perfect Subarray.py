ef57b5071acf402bdb7287b5bbf2669c79238ec0
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-05-18 09:37:35 +0530

    Create Perfect Subarray.py

---
+++ Kickstart 2020/Round C/Perfect Subarray.py
@@ -0,0 +1,13 @@
+import math
+t = int(input())
+for z in range(t):
+    n = int(input())
+    A = list(map(int,input().split()))
+    c = 0
+    for i in range(0,n):
+        for j in range(i+1,n+1):
+            x = sum(A[i:j])
+            sr = math.sqrt(x)
+            if(sr - math.floor(sr) == 0):
+                c+=1
+    print("Case #"+str(z+1)+": "+str(c))   
