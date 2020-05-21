ec540c85c76182d9a7b94e8003e7418b4b336e9b
Ojasvi Chauhan <500068394@stu.upes.ac.in>
2020-05-21 21:55:28 +0530

    Update Countdown.py

--- Kickstart 2020/Round C/Countdown.py
+++ Kickstart 2020/Round C/Countdown.py
@@ -1,11 +1,18 @@
+def countdown(A,n,k):
+    count = 0
+    for i in range(0,n-k+1):
+        temp = k
+        for j in range(i,k+i):
+            if(A[j]!= temp):
+                break
+            else:
+                if(temp == 1):
+                    count += 1
+                temp -= 1
+    return str(count)
+
 t = int(input())
 for z in range(t):
-    n,k = map(int,input().split())
-    A = list(map(int,input().split()))
-    l = []
-    for i in range(k,0,-1):
-        l.append(i)
-    s1 = ''.join(str(i) for i in A)
-    s2 = ''.join(str(i) for i in l)
-    c = s1.count(s2)
-    print("Case #"+str(z+1)+": "+str(c))
+    n,k = map(int, input().split())
+    A = list(map(int, input().split()))
+    print("Case #"+ str(z+1) + ": "+ countdown(A,n,k))
